0dab20af561b384e9bd60e58b160d984693c7325
chinsun9 <chin_sung@naver.com>
2021-09-16 00:11:36 +0900

    refactor: project structure

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 const express = require('express');
-const fs = require('fs');
-const app = express();
-const remove = require('./js/remove.js');
-const remove_html = require('./js/remove_html');
-const bodyParser = require('body-parser');
+const path = require('path');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-// css 폴더에 있는 정적인 파일들 가져와서 url 매핑
-app.use('/css', express.static('css'));
-// /js/remove.js url에 remove 파일 매핑
-app.use('/js/remove', remove);
-app.use('/js/remove_html', remove_html);
-// /경로에 index.html 파일을 매핑
+const app = express();
+
+const staticDir = path.join(__dirname, './public');
+const indexPage = path.join(staticDir, 'index.html');
+
+app.use(express.static(staticDir));
+
+app.use('/js/remove', require('./js/remove'));
+app.use('/js/remove_html', require('./js/remove_html'));
+
 app.get('/', function(req, res) {
-    fs.readFile('index.html', 'utf-8', function(error, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.end(data);
-    });
+    res.sendFile(indexPage)
 });
-//65001포트로 local 서버 open
+
 app.listen(65001, function() {
     console.log('Server Running at http://localhost:65001');
 });
