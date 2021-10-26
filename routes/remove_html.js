0865664f2731c3aaf81893ac78b4ce5da41c0eb0
chinsun9 <chin_sung@naver.com>
2021-10-26 23:22:40 +0900

    refactor: use Sync api, remove callback

--- routes/remove_html.js
+++ routes/remove_html.js
@@ -7,56 +7,51 @@ const router = express.Router();
 
 function removeComment(objectData) {
   const { originDirName, resultDirName } = objectData;
-  // 변환 파일이 저장될 디렉터리가 없으면 생성
-  if (!fs.existsSync(resultDirName)) {
-    fs.mkdir(resultDirName, () => {
-      console.log(resultDirName);
-    });
-  }
-  // origin 디렉터리 읽기
-  fs.readdir(originDirName, (error, filenames) => {
-    if (error) {
-      console.log(error);
-      return;
+
+  try {
+    // 변환 파일이 저장될 디렉터리가 없으면 생성
+    if (!fs.existsSync(resultDirName)) {
+      fs.mkdirSync(resultDirName);
     }
+
+    // origin 디렉터리 읽기
+    const filenames = fs.readdirSync(originDirName);
+
     // 하위파일들에 대한 반복문
     filenames.forEach((filename) => {
-      // 하위파일들의 형식을 확인
+      const targetFile = `${originDirName}\\${filename}`;
+      const targetOutputFile = `${resultDirName}\\${filename}`;
 
-      fs.stat(`${originDirName}\\${filename}`, (error2, stats) => {
-        if (error2) {
-          console.log(error2);
-        }
-        // 파일일 경우 처리
-        if (stats.isFile()) {
-          // javascript 파일일 경우 처리
-          if (path.extname(filename) === '.html') {
-            fs.readFile(`${originDirName}\\${filename}`, 'utf-8', (error3, data) => {
-              if (error3) {
-                console.log(error3);
-                return;
-              }
-              const temp3 = beautify(data, {
-                indent_size: objectData.indent_size,
-              });
-              // replace한 데이터를 다시 덮어쓰기
-              fs.writeFile(`${resultDirName}\\${filename}`, temp3, 'utf-8', () => {
-                console.log(`${resultDirName}\\${filename}`);
-              });
-            });
-          }
-        }
-        // 디렉터리일 경우 해당 디렉터리부터 다시 함수호출
-        if (stats.isDirectory()) {
-          removeComment({
-            ...objectData,
-            originDirName: `${originDirName}\\${filename}`,
-            resultDirName: `${resultDirName}\\${filename}`,
-          });
-        }
-      });
+      // 하위파일들의 형식을 확인
+      const stats = fs.statSync(targetFile);
+
+      // 파일일 경우 처리
+      if (stats.isFile() && path.extname(filename) === '.html') {
+        // javascript 파일일 경우 처리
+        const origin = fs.readFileSync(targetFile, 'utf-8');
+        const beautified = beautify(origin, {
+          indent_size: objectData.indent_size,
+        });
+          // replace한 데이터를 다시 덮어쓰기
+        fs.writeFileSync(targetOutputFile, beautified, 'utf-8');
+        console.log(`done: ${targetOutputFile}`);
+        return;
+      }
+
+      // 디렉터리일 경우 해당 디렉터리부터 다시 함수호출
+      if (stats.isDirectory()) {
+        removeComment({
+          ...objectData,
+          originDirName: targetFile,
+          resultDirName: targetOutputFile,
+        });
+      }
     });
-  });
+  } catch (error) {
+    console.log({ error });
+    return false;
+  }
+  return true;
 }
 
 function checkBody(req, res, next) {
@@ -90,9 +85,9 @@ function checkBody(req, res, next) {
 
 router.post('/', checkBody, (req, res) => {
   console.log({ data: req.data });
-  removeComment(req.data);
+  const isDone = removeComment(req.data);
   return res.json({
-    message: 'done',
+    message: isDone ? 'done' : 'fail',
   });
 });
 
