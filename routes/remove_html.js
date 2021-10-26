82d0b869a8b1fc63ae8a32c73699fdd1075fef0a
chinsun9 <chin_sung@naver.com>
2021-10-26 22:03:45 +0900

    use camelCase

--- routes/remove_html.js
+++ routes/remove_html.js
@@ -5,27 +5,18 @@ const express = require('express');
 
 const router = express.Router();
 
-let origin_dir_name;
-let result_dir_name;
+let originDirName;
+let resultDirName;
 
-router.post('/', (req, res) => {
-  checkBody(req, res).then((objectData) => {
-    res.redirect('/');
-    removeComment(origin_dir_name, result_dir_name, objectData);
-  }).catch(() => {
-    res.redirect('/');
-  });
-});
-
-function removeComment(origin_dir_name, result_dir_name, objectData) {
+function removeComment(originDirName2, resultDirName2, objectData) {
   // 변환 파일이 저장될 디렉터리가 없으면 생성
-  if (!fs.existsSync(result_dir_name)) {
-    fs.mkdir(result_dir_name, () => {
-      console.log(result_dir_name);
+  if (!fs.existsSync(resultDirName2)) {
+    fs.mkdir(resultDirName2, () => {
+      console.log(resultDirName2);
     });
   }
   // origin 디렉터리 읽기
-  fs.readdir(origin_dir_name, (error, filenames) => {
+  fs.readdir(originDirName2, (error, filenames) => {
     if (error) {
       console.log(error);
       return;
@@ -34,7 +25,7 @@ function removeComment(origin_dir_name, result_dir_name, objectData) {
     filenames.forEach((filename, index) => {
       // 하위파일들의 형식을 확인
 
-      fs.stat(`${origin_dir_name}\\${filename}`, (error, stats) => {
+      fs.stat(`${originDirName2}\\${filename}`, (error, stats) => {
         if (error) {
           console.log(error);
         }
@@ -42,7 +33,7 @@ function removeComment(origin_dir_name, result_dir_name, objectData) {
         if (stats.isFile()) {
           // javascript 파일일 경우 처리
           if (path.extname(filename) == '.html') {
-            fs.readFile(`${origin_dir_name}\\${filename}`, 'utf-8', (error, data) => {
+            fs.readFile(`${originDirName2}\\${filename}`, 'utf-8', (error, data) => {
               if (error) {
                 console.log(error);
                 return;
@@ -51,15 +42,15 @@ function removeComment(origin_dir_name, result_dir_name, objectData) {
                 indent_size: objectData.indent_size,
               });
               // replace한 데이터를 다시 덮어쓰기
-              fs.writeFile(`${result_dir_name}\\${filename}`, temp3, 'utf-8', () => {
-                console.log(`${result_dir_name}\\${filename}`);
+              fs.writeFile(`${resultDirName2}\\${filename}`, temp3, 'utf-8', () => {
+                console.log(`${resultDirName2}\\${filename}`);
               });
             });
           }
         }
         // 디렉터리일 경우 해당 디렉터리부터 다시 함수호출
         if (stats.isDirectory()) {
-          removeComment(`${origin_dir_name}\\${filename}`, `${result_dir_name}\\${filename}`, objectData);
+          removeComment(`${originDirName2}\\${filename}`, `${resultDirName2}\\${filename}`, objectData);
         }
       });
     });
@@ -68,29 +59,38 @@ function removeComment(origin_dir_name, result_dir_name, objectData) {
 
 function checkBody(req, res) {
   return new Promise((resolve, reject) => {
-    const data = new Object();
+    const data = {};
     data.indent_size = 4;
-    if (req.body.origin_dir_name != '') {
-      origin_dir_name = req.body.origin_dir_name;
+    if (req.body.origin_dir_name !== '') {
+      originDirName = req.body.origin_dir_name;
     } else {
       reject();
     }
-    if (req.body.result_dir_name != '') {
-      result_dir_name = req.body.result_dir_name;
+    if (req.body.result_dir_name !== '') {
+      resultDirName = req.body.result_dir_name;
     } else {
       reject();
     }
-    if (req.body.pattern != '' && req.body.pattern != null) {
+    if (req.body.pattern !== '' && req.body.pattern !== null) {
       data.pattern = req.body.pattern;
     }
-    if (req.body.pattern_type != '' && req.body.pattern_type != null) {
+    if (req.body.pattern_type !== '' && req.body.pattern_type !== null) {
       data.pattern_type = req.body.pattern_type;
     }
-    if (req.body.indent_size != '' && req.body.indent_size != null) {
+    if (req.body.indent_size !== '' && req.body.indent_size !== null) {
       data.indent_size = req.body.indent_size;
     }
     resolve(data);
   });
 }
+
+router.post('/', (req, res) => {
+  checkBody(req, res).then((objectData) => {
+    res.redirect('/');
+    removeComment(originDirName, resultDirName, objectData);
+  }).catch(() => {
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
